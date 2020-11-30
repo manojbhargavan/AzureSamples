be51cb6c06cb80e329f1dcd52c0e6b35985839dc
Manoj Bhargavan <manojbhargavan07@gmail.com>
2020-11-30 20:20:39 +0530

    Storage Account Container & Table Basics

--- Storage/AzureStorageWrapper/Constants.cs
+++ Storage/AzureStorageWrapper/Constants.cs
@@ -9,6 +9,6 @@ namespace AzureStorageWrapper
     public static class Constants
     {
         public static string StorageAccountConnectionString { get; } 
-            = "DefaultEndpointsProtocol=https;AccountName=demostore7856;AccountKey=En7FlAc6aqxVxIs6WL91jlpaJ6fr2Wlkvh+DFb2g5Dr+dwTHjYBt+AMX46V7yjb2ccwhtp4Rpo0wuQAVNR3oBA==;EndpointSuffix=core.windows.net";
+            = "****";
     }
 }
